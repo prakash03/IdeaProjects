cc97a678eb1824a7928a724652ff7eff90fbb286
PRAKASH <prakashkantheti@gmail.com>
2018-06-18 15:30:57 -0400

    Updated ReverseWords.java

--- src/InterviewPractice/ReverseWords.java
+++ src/InterviewPractice/ReverseWords.java
@@ -1,5 +1,19 @@
 package InterviewPractice;
 
 public class ReverseWords {
-    
+
+    static String reverseWordsInSentence(String str){
+
+        String answer = "";
+        String[] inputArray = str.split(" ");
+        for(int i=inputArray.length-1; i>=0; i--){
+            answer += inputArray[i] + " ";
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        String sentence1 = "Going good! Reverse this sentence";
+        String sentence2 = " ";
+        System.out.println(reverseWordsInSentence(sentence2));
+    }
 }
